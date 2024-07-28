6b0161d03d1f8adbc4ac1a0ff62062a9d493c54d
Karthi <Karthi@DESKTOP-NHOM0OS>
2024-07-27 20:05:19 -0700

    ggg

---
+++ Hardik/src/com/tns/Nin.java
@@ -0,0 +1,14 @@
+package com.tns;
+
+public class Nin {
+
+	public static void main(String[] args) {
+		Vehicle B=new Vehicle();
+		Bike C=new Bike();
+		B.add();
+		C.print();
+	
+
+	}
+
+}
---
+++ Hardik/src/com/tns/Vehicle.java
@@ -0,0 +1,20 @@
+package com.tns;
+
+
+public class Vehicle {
+
+	void add() {
+		System.out.println("two wheeler");
+	}
+}
+ class Bike extends Vehicle{
+	 
+	void print() {
+	System.out.println("duke");
+	}
+ }
+ class Car extends Vehicle{
+	void display() {
+		System.out.println("suzuki");
+	}
+}
