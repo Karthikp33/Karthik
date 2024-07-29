5ef196c40183c537587bf64e09a357dc421c1d88
Karthi <Karthi@DESKTOP-NHOM0OS>
2024-07-29 00:23:15 -0700

    cfdvre

---
+++ Java/src/com/Interfaces/A.java
@@ -0,0 +1,28 @@
+package com.Interfaces;
+
+public interface A {
+	abstract void a();
+	abstract void b();
+	 abstract void c();
+}
+class B implements A{
+
+	@Override
+	public void a() {
+		System.out.println("duke");
+		}
+
+	@Override
+	public void b() {
+		System.out.println("rc");
+		
+	}
+
+	@Override
+	public void c() {
+		System.out.println("ktm");
+		
+	}
+		
+	}
+
---
+++ Java/src/com/Interfaces/Arr.java
@@ -0,0 +1,14 @@
+package com.Interfaces;
+
+public class Arr {
+
+	public static void main(String[]args) {
+		B cc=new B();
+		cc.b();
+		cc.a();
+		cc.c();
+		
+
+	}
+
+}
