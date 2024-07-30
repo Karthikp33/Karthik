fc7243540e23446a2fea902fe04e7953bef0c8f5
Karthi <Karthi@DESKTOP-NHOM0OS>
2024-07-30 08:54:34 -0700

    asdfg

---
+++ Java/src/com/Array/A.java
@@ -0,0 +1,11 @@
+package com.Array;
+
+public class A {
+	void add() {
+for(int i=0;i<=3;i++) {
+	 for(int j=0;j<=3;j++) {
+	}
+
+}
+}
+}
---
+++ Java/src/com/Array/Main.java
@@ -0,0 +1,11 @@
+package com.Array;
+
+public class Main {
+
+	public static void main(String[] args) {
+		A bb=new A();
+		bb.add();
+
+	}
+
+}
